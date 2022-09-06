6f7e48f7bc5ba5037d8a7bb073a3c13efe4ce045
Olga Nadongo <winnienadongo@gmail.com>
2022-09-06 11:39:22 +0300

    seeding data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/users" do
+    { message: "Good luck with your project!" }.to_json
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+puts "Creating user..."
+User.create(email:"willowj@email.com", password_digest:"janera")
+User.create(email:"amina@email.com", password_digest:"amina")
+
+puts "Creating projects..."
+Project.create(title:"Excavation")
+Project.create(title:"Research Land")
+
+puts "Creating teammates ..."
+Teammate.create(name:"Miya", email:"miya@yahoo.com")
+Teammate.create(name:"Zia", email:"zia@gmail.com")
+
+puts "Creating teammate_projects"
+ teammate_projects.create()
 
 puts "✅ Done seeding!"
