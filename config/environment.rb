4c3ceeed526134408594798521e6a936db7ef4c6
Olga Nadongo <winnienadongo@gmail.com>
2022-09-12 08:45:21 +0300

    edit env

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,11 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['RACK_ENV']}.sqlite"
+ )
+
 # Require in all files in 'app' directory
 require_all 'app'
