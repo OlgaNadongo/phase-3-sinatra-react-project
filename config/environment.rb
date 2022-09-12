8f5a4c4cee08c31a25ee88f6ea403d17929ad0c3
Olga Nadongo <winnienadongo@gmail.com>
2022-09-12 09:35:10 +0300

    change env

--- config/environment.rb
+++ config/environment.rb
@@ -2,9 +2,16 @@
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "production"
 
+require 'active_record'
 # Require in Gems
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
+
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['RACK_ENV']}.sqlite"
+ )
 
 # Require in all files in 'app' directory
 require_all 'app'
